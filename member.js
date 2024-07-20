fdddfdc54b924e9a52c8459c192c5a99dfaf4883
Julio Arana Jr. <113112001+julioaranajr@users.noreply.github.com>
2024-07-20 22:12:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = {
+        name: 'John',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 2000
+    };
+    return skills;
+}
+module.exports = skillsMember;
